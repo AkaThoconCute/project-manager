d06028cdc7f8268220f4af3d1286bd76f3b2a583
Derek <nhannguyen291219@outlook.com>
2026-04-05 16:48:48 +0700

    Epic 9.3 — Remaining Pages

--- kanban-board-fe/src/components/pages/NotFoundPage.tsx
+++ kanban-board-fe/src/components/pages/NotFoundPage.tsx
@@ -1,2 +1,46 @@
-const NotFoundPage = () => <div>NotFoundPage placeholder</div>;
+import { Link } from "react-router-dom";
+import { Box, Button } from "@mui/material";
+import Helmet from "../Helmet";
+
+const NotFoundPage: React.FC = () => {
+  return (
+    <Box
+      sx={{
+        margin: "25vh auto 0 auto",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        fontSize: "3.5em",
+        padding: "10px",
+        "& h6": {
+          margin: 0,
+          color: "#a4a4a4",
+          fontWeight: 100,
+          fontSize: "1.2rem",
+          textAlign: "center",
+        },
+        "& h2": {
+          margin: 0,
+          color: "#fff",
+          fontSize: { xs: "1.2em" },
+        },
+      }}
+    >
+      <Helmet title="Page not found" />
+      <h2>404...</h2>
+      <h6 style={{ maxWidth: 500 }}>
+        Such page does not exist. Head back to your projects!
+      </h6>
+      <Link
+        to="/boards"
+        style={{ textDecoration: "none", marginTop: "10px", fontSize: 0 }}
+      >
+        <Button color="primary" variant="outlined" size="large">
+          Boards
+        </Button>
+      </Link>
+    </Box>
+  );
+};
+
 export default NotFoundPage;
--- kanban-board-fe/src/components/pages/ProjectJoinPage.tsx
+++ kanban-board-fe/src/components/pages/ProjectJoinPage.tsx
@@ -1,2 +1,42 @@
-const ProjectJoinPage = () => <div>ProjectJoinPage placeholder</div>;
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { BACKGROUND_COLORS } from "../../util/colorsConstants";
+import { useAppSelector } from "../../redux/hooks";
+import Loader from "../Loader";
+
+const ProjectJoinPage: React.FC = () => {
+  const { socket } = useAppSelector((state) => state.socketConnection);
+  const { userInfo } = useAppSelector((state) => state.userLogin);
+  const { projectId, joinId } = useParams<{
+    projectId: string;
+    joinId: string;
+  }>();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (socket && projectId && joinId && userInfo) {
+      let projectJoined = userInfo.projectsJoined.findIndex(
+        (x) => x._id === projectId,
+      );
+      if (projectJoined === -1) {
+        projectJoined = userInfo.projectsCreated.findIndex(
+          (x) => x._id === projectId,
+        );
+      }
+      if (projectJoined !== -1) {
+        navigate(`/project/${projectId}`);
+      } else {
+        const background =
+          BACKGROUND_COLORS[Math.floor(Math.random() * Math.floor(5))];
+
+        socket.emit("project-join", { projectId, joinId, background }, () =>
+          navigate(`/project/${projectId}`),
+        );
+      }
+    }
+  }, [socket, navigate, userInfo, projectId, joinId]);
+
+  return <Loader />;
+};
+
 export default ProjectJoinPage;
